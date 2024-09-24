5434bad1c42435fbcb9bdb4c8246949fc5ac0e39
HongGit <soojam@konkuk.ac.kr>
2024-09-25 02:39:31 +0900

    [FEAT/#13] 최대 배송날짜 조회, 수정 API 구현

--- src/main/java/com/rootandfruit/server/controller/DeliveryController.java
+++ src/main/java/com/rootandfruit/server/controller/DeliveryController.java
@@ -4,7 +4,9 @@ import com.rootandfruit.server.service.DeliveryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,20 @@ public class DeliveryController {
             @RequestBody List<Long> deliveryInfoIds
     ) {
         deliveryService.statusCompletedToShipped(deliveryInfoIds);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("delivery/max")
+    public ResponseEntity<Integer> getAllowedDeliveryDay() {
+        return ResponseEntity.ok(deliveryService.getAllowedDay());
+    }
+
+    @PatchMapping("delivery/{allowedDeliveryDays}")
+    public ResponseEntity<Void> changeDay(
+            @PathVariable int allowedDeliveryDays
+    ) {
+
+        deliveryService.changeAllowedDay(allowedDeliveryDays);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/rootandfruit/server/service/DeliveryService.java
+++ src/main/java/com/rootandfruit/server/service/DeliveryService.java
@@ -2,10 +2,12 @@ package com.rootandfruit.server.service;
 
 import com.rootandfruit.server.domain.DeliveryInfo;
 import com.rootandfruit.server.domain.DeliveryStatus;
+import com.rootandfruit.server.domain.OrderMetaData;
 import com.rootandfruit.server.domain.Product;
 import com.rootandfruit.server.global.exception.CustomException;
 import com.rootandfruit.server.global.exception.ErrorType;
 import com.rootandfruit.server.repository.DeliveryInfoRepository;
+import com.rootandfruit.server.repository.OrderMetaDataRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeliveryService {
 
     private final DeliveryInfoRepository deliveryInfoRepository;
+    private final OrderMetaDataRepository orderMetaDataRepository;
 
     @Transactional
     public void statusCompletedToShipped(List<Long> deliveryInfoIds) {
@@ -24,5 +27,17 @@ public class DeliveryService {
             throw new CustomException(ErrorType.NOT_FOUND_DELIVERY_ERROR);
         }
         deliveryInfos.forEach(deliveryInfo -> deliveryInfo.changeDeliveryStatus(DeliveryStatus.ORDER_SHIPPED));
+    }
+
+    @Transactional(readOnly = true)
+    public int getAllowedDay() {
+        OrderMetaData orderMetaData = orderMetaDataRepository.findOrderMetaDataByIdOrThrow(1L);
+        return orderMetaData.getAllowedDeliveryDays();
+    }
+
+    @Transactional
+    public void changeAllowedDay(int allowedDeliveryDays) {
+        OrderMetaData orderMetaData = orderMetaDataRepository.findOrderMetaDataByIdOrThrow(1L);
+        orderMetaData.changeAllowedDeliveryDays(allowedDeliveryDays);
     }
 }

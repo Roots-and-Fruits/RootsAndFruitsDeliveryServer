83255bfe6cc9b5a4c427ace2a2fc323317b432be
HongGit <soojam@konkuk.ac.kr>
2024-09-26 02:08:43 +0900

    [FEAT] 스웨서 HTTPS로 요청 변경

--- src/main/java/com/rootandfruit/server/RootAndFruitServerApplication.java
+++ src/main/java/com/rootandfruit/server/RootAndFruitServerApplication.java
@@ -1,8 +1,11 @@
 package com.rootandfruit.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://server.yeolmae.store", description = "product yeolmae server")})
 @SpringBootApplication
 public class RootAndFruitServerApplication {
 

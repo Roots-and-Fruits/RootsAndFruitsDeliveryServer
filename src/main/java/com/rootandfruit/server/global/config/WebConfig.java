94ad5c266e792c91b66d759d6baa53d024a06a2c
HongGit <soojam@konkuk.ac.kr>
2024-09-26 01:37:09 +0900

    [FIX] CORS 대응

--- src/main/java/com/rootandfruit/server/global/config/WebConfig.java
+++ src/main/java/com/rootandfruit/server/global/config/WebConfig.java
@@ -9,11 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173",
-                        "https://server.yeolmae.store")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }

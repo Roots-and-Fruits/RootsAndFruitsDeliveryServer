c626838c1a6f41c28681df862835ff2a8cd32013
HongGit <soojam@konkuk.ac.kr>
2024-09-25 19:35:22 +0900

    [HOTFIX] WebConfig에 서브 도메인 추가

--- src/main/java/com/rootandfruit/server/global/config/WebConfig.java
+++ src/main/java/com/rootandfruit/server/global/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.rootandfruit.server.global.config;
 
-import java.util.List;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173",
+                        "https://server.yeolmae.store")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)

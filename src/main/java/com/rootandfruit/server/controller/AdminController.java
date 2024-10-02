d7a1ac1760a7e92b04a969d027d39636f59129ec
HongGit <soojam@konkuk.ac.kr>
2024-10-02 16:25:26 +0900

    [FEAT/#24] Admin 컨트롤러 구현

---
+++ src/main/java/com/rootandfruit/server/controller/AdminController.java
@@ -0,0 +1,35 @@
+package com.rootandfruit.server.controller;
+
+import com.rootandfruit.server.controller.docs.AdminControllerDocs;
+import com.rootandfruit.server.dto.AdminAuthenticateRequestDto;
+import com.rootandfruit.server.dto.AdminCreateRequestDto;
+import com.rootandfruit.server.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController implements AdminControllerDocs {
+    private final AdminService adminService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createAdmin(
+            @RequestBody AdminCreateRequestDto adminCreateRequestDto
+    ) {
+        adminService.createAdmin(adminCreateRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<Void> authenticateAdmin(
+            @RequestBody AdminAuthenticateRequestDto adminAuthenticateRequestDto
+            ) {
+        adminService.authenticateAdmin(adminAuthenticateRequestDto);
+        return ResponseEntity.ok().build();
+    }
+}

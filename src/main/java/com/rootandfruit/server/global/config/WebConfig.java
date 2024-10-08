376bf5e8a72694ea98a313e5d7714069104f06c1
HongGit <soojam@konkuk.ac.kr>
2024-10-08 17:53:06 +0900

    [HOTFIX] 리액트 로컬호스트 요청 허용

--- src/main/java/com/rootandfruit/server/global/config/WebConfig.java
+++ src/main/java/com/rootandfruit/server/global/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("*", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .maxAge(3600);
     }

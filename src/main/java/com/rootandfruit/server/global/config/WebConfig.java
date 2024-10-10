da583f59afdf788a043d44b36a8e574cb215355f
HongGit <soojam@konkuk.ac.kr>
2024-10-10 15:45:36 +0900

    [HOTFIX] 요청 전체 도메인 허용

--- src/main/java/com/rootandfruit/server/global/config/WebConfig.java
+++ src/main/java/com/rootandfruit/server/global/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .maxAge(3600);
     }

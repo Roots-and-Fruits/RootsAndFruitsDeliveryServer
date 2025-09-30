541d59d0448f6cf6aac015ed03ea1d4b81eab3c7
HongGit <soojam@konkuk.ac.kr>
2025-09-30 21:36:08 +0900

    fix: CORS url 변경

--- src/main/java/com/rootandfruit/server/global/config/WebConfig.java
+++ src/main/java/com/rootandfruit/server/global/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*", "http://localhost:5173", "https://yeolmae.store")
+                .allowedOrigins("*", "http://localhost:5173", "https://yeolmae.shop")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .maxAge(3600);
